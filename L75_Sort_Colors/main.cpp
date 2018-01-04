464be48a39a9be5984a862a4657948ed8c99699f
he <ffpphh@outlook.com>
2018-01-04 20:38:54 +0800

    75

---
+++ L75_Sort_Colors/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int num_count[3] = {0, 0, 0};
+        for (auto num:nums){
+            num_count[num]++;
+        }
+        int index = 0;
+        while (num_count[0] > 0){
+            nums[index++] = 0;
+            num_count[0]--;
+        }
+        while (num_count[1] > 0){
+            nums[index++] = 1;
+            num_count[1]--;
+        }
+        while (num_count[2] > 0){
+            nums[index++] = 2;
+            num_count[2]--;
+        }
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
